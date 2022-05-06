72348718d4a14d3274ba8c9579911984791fc60a
Diego Garcia <diego.garciamellado@gmail.com>
2022-05-05 21:39:28 -0300

    Primer entrega proyecto final js

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 alert("Bienvenido al Sistema de turno online");
 
-let registro=prompt("Se encuentra Registrado? s/n"); 
+
+
+let admin=prompt("Es Utd Administrador? s/n");
+if (admin==="s" || admin === "S"){
+    let userAdmin = prompt ("Ingrese Usuario Administrador: ");
+    let passAdmin = prompt ("Ingrese Contarseña: ");
+        if (userAdmin==="admin" && passAdmin==="admin"){
+            alert("Sistema para agregar turnos");
+            const turnos = ["08:00", "10:00", "12:00"];
+             let turno = prompt("agregue un turno");
+                alert(`"Usted a agregado el turno" ${turno} hs`)
+        }
+        if (userAdmin !="admin" && passAdmin !="admin"){
+            alert("El usuario o contraseña no coinciden")
+        }
+    }
+if (admin==="n" || admin ==="N"){
+    let registro=prompt("Se encuentra Registrado? s/n"); 
+
 
 //REGISTRO
 if(registro=="n" || registro=="N"){
-    // let usuario=prompt("Ingrese nombre de usuario");
-    // let contraseña=prompt("Ingrese su contraseña:");
-    // let confirm=prompt("Vuelva a ingresar la contraseña: ")
-    // if(contraseña===confirm){
-    //     alert("Registro correcto!");
-    // }
+   
     function Registro(usuario, contraseña, contraseña2){
        this.usuario = usuario
        this.contraseña = contraseña
@@ -21,7 +34,9 @@ if(registro=="n" || registro=="N"){
     let contraseña2 = prompt("vuelva a ingresar su contraseña")
     if(contraseña===contraseña2){
         alert("registro correcto!");
+        
     }
+    
     else{
         alert("Las contraseñas no coinciden")
         
@@ -34,17 +49,17 @@ if(registro=="n" || registro=="N"){
 
 
     while(otro=="n"){
-        let horario=prompt("Ingrese el horario que desea asistir (08:00 , 10:00 , 12:00): ");
-        let profe=prompt("Ingrese con que profesora quiere tomar la clase (Diana , Jose)")
-        alert (`"Usted eligio el horario" ${horario} con la profesora ${profe}`)
-     
-        otro=prompt("Desea elegir otro horario?s/n");
-
+        const arr1 = ["08:00 - profe diana","10:00 profe jose", "12:00 - Profe Diana"];
+            alert(`"los horairos disponibles son " ${arr1}`)
+        let horario=prompt("Ingrese el horario que desea asistir (08:00 - Profe Diana , 10:00 - Profe Jose, 12:00 - Profe Diana): ");
+            alert(`"usted eligio el turno" ${horario}`)
         
 
+     
+        otro=prompt("Desea elegir otro horario?s/n");
     }
 
-    
+    otro=prompt("Desea elegir otro horario?s/n");
 
     
 
@@ -62,19 +77,16 @@ if(registro=="s" || registro=="S"){
     
 
     while(otro=="s"){
-        let horario=prompt("Ingrese el horario que desea asistir (08:00 , 10:00 , 12:00): ");
-        let profe=prompt("Ingrese con que profesora quiere tomar la clase (Diana , Jose")
-        alert (`"Usted eligio el horario" ${horario} con la profesora ${profe}`)
+        const arr1 = ["08:00 - profe diana","10:00 profe jose", "12:00 - Profe Diana"];
+            alert(`"los horairos disponibles son " ${arr1}`)
+        let horario=prompt("Ingrese el horario que desea asistir (08:00 - Profe Diana , 10:00 - Profe Jose, 12:00 - Profe Diana): ");
+            alert(`"usted eligio el turno" ${horario}`)
+        
+
      
         otro=prompt("Desea elegir otro horario?s/n");
 
     }
    
 }
-// //Elegir turno
-// function solicitarhorario(){
-//     let horarioIngresado = prompt("ingresehorario")
-//     alert( `El horario elegido es ${horarioIngresado}`)
-// }
-
-// solicitarhorario()
+}
