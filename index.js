c85bb5b7cab7f222ce6f2e0ece6cf2698873dac2
Diego Garcia <diego.garciamellado@gmail.com>
2022-05-12 22:39:51 -0300

    Segunda version proyecto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert("Bienvenido al Sistema de turno online");
+/*alert("Bienvenido al Sistema de turno online");
 
 
 
@@ -89,4 +89,93 @@ if(registro=="s" || registro=="S"){
     }
    
 }
+}*/
+
+
+
+const contenedor = document.getElementById("turnos");
+
+const tablaCarrito = document.getElementById("tablaCarrito");
+
+const carrito = [];
+
+const TURNOS =[
+   
+{   
+    id: 1,
+    nombre: "Turno 08:00 hs",
+    profesora: "Diana",
+    lugares: "Lugares Disponibles: 3",
+    imagen: "https://dw24840-diegogarciamellado.000webhostapp.com/img/nosotros_2.png"
+
+},
+{
+    id: 2,
+    nombre: "Turno 10:00 hs",
+    profesora: "Josefina",
+    lugares: "Lugares Disponibles: 5",
+    imagen: "https://dw24840-diegogarciamellado.000webhostapp.com/img/nosotros_1.png",
+
+},
+];
+
+const getCard = (item) => {
+    return (`
+    <div class="card" style="width: 18rem;">
+        <img src="${item.imagen}" class="card-img-top" alt="${item.nombre}">
+         <div class="card-body">
+            <h5 class="card-title">${item.nombre}</h5>
+            <h5 class="card-title">${item.profesora}</h5>
+            <p class="card-text">${item.lugares}</p>
+            <a href="#" onclick=reservarClase(${item.id}) class="btn btn-primary">Reservar Turno</a>
+    </div>
+  </div>
+    `)
+};
+
+const getRow = (item) => {
+    return(
+        `
+        <tr>
+            <th scope="row">${item.id}</th>
+            <td>${item.nombre}</td>
+            <td>${item.profesora}</td>
+            <td>${item.lugares}</td>
+            <td><img style="width:40px" src=${item.imagen} alt"imagen"> </td>
+        </tr>
+
+        `
+    )
 }
+
+const cargarTurnos = (datos, nodo, esTabla) => {
+    let acumulador = "";
+    datos.forEach((el) => {
+        acumulador += esTabla ? getRow(el) : getCard(el);
+    })
+    nodo.innerHTML = acumulador;
+};
+
+const reservarClase = (id) =>{
+    const seleccion = TURNOS.find(item => item.id===id)
+
+    carrito.push({
+        id: seleccion.id,
+        nombre: seleccion.nombre,
+        profesora: seleccion.profesora,
+        lugares: seleccion.lugares,
+        imagen: seleccion.imagen,
+    })
+
+    cargarTurnos(carrito, tablaCarrito, true);
+    
+
+
+    
+}
+
+cargarTurnos(TURNOS, contenedor, false);
+
+
+
+
